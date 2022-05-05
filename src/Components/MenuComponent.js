148e836dbdb5d5cbfd0914b3953eb141e0e9c80b
Minh Bui <minhbui.sgn@gmail.com>
2022-05-05 22:05:15 +0700

    LAB04

---
+++ src/Components/MenuComponent.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import Detail from './DishdetailComponent';
+
+export default class Menu extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+
+        const menu = this.props.dishes.map((dish) => {
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <Card onClick={()=>this.props.onClick(dish.id)}>
+                        <CardImg width='100%' src={dish.image} alt={dish.name}/>
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+        
+        return (
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+        );
+    };
+}
