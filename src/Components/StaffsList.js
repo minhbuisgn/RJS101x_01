13d8e90821bc9a3a3788e69e163e5ecf07f755dd
Minh Bui <minhbui.sgn@gmail.com>
2022-07-27 03:05:57 +0700

    add search STAFF

--- src/Components/StaffsList.js
+++ src/Components/StaffsList.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Card, CardText } from 'reactstrap';
+import { Card, CardText, Button, Input } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { bindActionCreators } from "redux";
 
 function RenderStaffs({staff}) {
     return (
@@ -13,30 +14,71 @@ function RenderStaffs({staff}) {
     )
  }
 
-const StaffsList = (props) => {
+class StaffsList extends React.Component {
 
-    const list = props.staffs.map((staff) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nameFind : ""
+        };
+
+        this.searchStaff = this.searchStaff.bind(this);
+    }
+
+    searchStaff(event){
+        const nameSearch = event.target.nameSearch.value;
+        event.preventDefault();
+        this.setState({ nameFind: nameSearch });
+    }
+    
+    render() {
+        
+        const list = this.props.staffs
+            .filter ((val)=>{
+                if (this.state.nameFind === "") 
+                    return val;
+                else if (val.name.toLowerCase().includes(this.state.nameFind.toLowerCase()))
+                    return val;
+            })
+            .map((val) => {
+                return(
+                    <div key={val.id} className="col-6 col-md-4 col-lg-3 my-2">
+                        <RenderStaffs staff={val} />
+                    </div>
+                )
+            })
+    
         return (
-            <div key={staff.id} className="col-6 col-md-4 col-lg-3 my-2">
-                <RenderStaffs staff={staff} />
-            </div>
-        )
-    })
-
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <br/>
-                    <h3>DANH SÁCH NHÂN VIÊN</h3>
-                    <hr />
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <br/>
+                        <h3>DANH SÁCH NHÂN VIÊN</h3>
+                    </div>
+                    <form onSubmit={this.searchStaff} className="form-group row pt-4">
+                        <div className="col-8 col-md-8">
+                            <input type='text'
+                                name="nameSearch"
+                                className="form-control"
+                                placeholder="Nhập vào tên nhân viên cần tìm kiếm"
+                            />
+                        </div>
+                        <div className="col-4 col-md-4">
+                            <button className="btn btn-primary" type="submit">
+                                Tìm Kiếm
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <hr/>
+                <div className="row">
+                    {list}
                 </div>
             </div>
-            <div className="row">
-                {list}
-            </div>
-        </div>
-    )
+        )
+    }
+   
 }
 
 export default StaffsList;
+ 
